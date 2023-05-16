798e74528c0d0ee9267fc52834e1cba3fda2c737
s.yasser@ <s.yasser@chesstrending.com>
2023-05-16 14:57:32 +0300

    Face id code

---
+++ FaceID Test/NextViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NextViewController.swift
+//  FaceID Test
+//
+//  Created by Shrouk Yasser on 16/05/2023.
+//
+
+import Foundation
+import UIKit
+
+class NextViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
--- FaceID Test/ViewController.swift
+++ FaceID Test/ViewController.swift
@@ -1,19 +1,39 @@
-//
-//  ViewController.swift
-//  FaceID Test
-//
-//  Created by Shrouk Yasser on 16/05/2023.
-//
-
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
+
+    @IBAction func faceIDButtonPressed(_ sender: UIButton) {
+        let context = LAContext()
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let localizedReason = "Authenticate with Face ID to proceed"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: localizedReason) { [weak self] (success, error) in
+                if success {
+                    print("Authentication succeeded")
+                    DispatchQueue.main.async {
+                        let nextViewController = self?.storyboard?.instantiateViewController(withIdentifier: "NextViewController") as! NextViewController
+                        self?.present(nextViewController, animated: true, completion: nil)
+                    }
+                } else {
+                    print("Authentication failed")
+                    if let error = error as NSError? {
+                        if error.code == LAError.userCancel.rawValue {
+                            print("Authentication canceled by user")
+                        } else {
+                            print("Authentication error: \(error.localizedDescription)")
+                        }
+                    }
+                }
+            }
+        } else {
+            print("Face ID is not available or not enabled")
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
